56826b736110fa3ae4a8a4727a8fcd78c1e6bd9b
kbjid17 <48487303+kbjid17@users.noreply.github.com>
2021-12-27 10:54:01 +0900

    연속합 알고리즘 : dp[i] = Math.max(dp[i-1] + ar[i], ar[i]);

--- src/prob_1912_연속합_1227/Main_3_시간초과.java
+++ src/prob_1912_연속합_1227/Main_3_시간초과.java
@@ -1,33 +1,37 @@
-package prob_1912_연속합_미완;
+package prob_1912_연속합_1227;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_시간초과 {
-
-	static int N;
+public class Main_3_시간초과 {
 	static int[] ar;
-	static int num = Integer.MIN_VALUE,numb;
-	static StringBuilder sb = new StringBuilder();
+	static long[] dp;
+	static long ans = Long.MIN_VALUE;
+	static int N;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		N = Integer.parseInt(br.readLine());
 		
 		ar = new int[N];
+		dp = new long[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		for (int i = 0; i < N; i++) {
 			ar[i] = Integer.parseInt(st.nextToken());
+			dp[i] = ar[i];
+			ans = Math.max(ans, dp[i]);
 		}
+		
+		
 		for (int i = 0; i < N; i++) {
-			numb = ar[i];
 			for (int j = i+1; j < N; j++) {
-				num = Math.max(num, numb);
-				numb += ar[j];
-				
+				dp[i] += ar[j];
+				ans = Math.max(ans, dp[i]);
 			}
 		}
-		sb.append(num);
-		System.out.println(sb);
+		System.out.println(ans);
 	}
+
 }
--- src/prob_1912_연속합_1227/Main_4_해결.java
+++ src/prob_1912_연속합_1227/Main_4_해결.java
@@ -1,12 +1,12 @@
-package prob_1912_연속합_미완;
+package prob_1912_연속합_1227;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_2_메모리초과 {
+public class Main_4_해결 {
 	static int[] ar;
-	static long[][] dp;
+	static long[] dp;
 	static long ans = Long.MIN_VALUE;
 	static int N;
 	public static void main(String[] args) throws Exception{
@@ -15,23 +15,20 @@ public class Main_2_메모리초과 {
 		N = Integer.parseInt(br.readLine());
 		
 		ar = new int[N];
-		dp = new long[100000][100000];
+		dp = new long[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		for (int i = 0; i < N; i++) {
 			ar[i] = Integer.parseInt(st.nextToken());
-			dp[i][i] = ar[i];
-			ans = Math.max(ans, dp[i][i]);
 		}
 		
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = i+1; j < N; j++) {
-				dp[i][j] = dp[i][j-1]+ar[j];
-				ans = Math.max(ans, dp[i][j]);
-			}
+		dp[0] = ar[0];
+		ans = Math.max(dp[0], ans);
+		for (int i = 1; i < N; i++) {
+				dp[i] = Math.max(dp[i-1] + ar[i], ar[i]); // 연속 합 알고리즘
+				// 
+				ans = Math.max(dp[i], ans);
 		}
 		System.out.println(ans);
 	}
-
 }
