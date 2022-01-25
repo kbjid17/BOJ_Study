2b845a3b7855d961fb85e504d318cdd645a682e0
Your Name <someone@aaa.com>
2022-01-26 08:27:02 +0900

    220126 commit - 구현 알고리즘 단계에 추가된 문제 해결

---
+++ src/prob_1998년생인내가태국에선2541년생_18108_220126/Main.java
@@ -0,0 +1,17 @@
+package prob_1998년생인내가태국에선2541년생_18108_220126;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int n;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		n = Integer.parseInt(br.readLine());
+		
+		System.out.println(n-543);
+
+	}
+
+}
