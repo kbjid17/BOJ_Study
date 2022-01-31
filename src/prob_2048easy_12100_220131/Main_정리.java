b79530398cdec2c2560fdd4becfce454d924cb81
Your Name <someone@aaa.com>
2022-01-31 15:11:13 +0900

    220131 commit - 가지치기 안했는데 풀림.. 성장한건가?

--- src/prob_2048easy_12100_220131/Main_정리.java
+++ src/prob_2048easy_12100_220131/Main_정리.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main_정리 {
 	static int N,max = Integer.MIN_VALUE;
 	static int[][] ar;
 	static Stack<block> stack = new Stack<>();
@@ -26,8 +26,6 @@ public class Main {
 			move(5,1,ar);
 			move(5,2,ar);
 			move(5,3,ar);
-			
-			
 			System.out.println(max);
 	}
 	private static void move(int count,int dir,int[][] ars) {
@@ -38,17 +36,6 @@ public class Main {
 				copyar[i][j] = ars[i][j];
 			}
 		}
-
-		//이동 알고리즘 구현
-		/*
-		 1. 0이 아닌 수를 발견(block)
-		 2. 그 블록의 y,x,val을 저장한 후 그자리는 0으로 만듦
-		 3. 저장된 값을 stack에 삽입
-		 4. dfs로 갈 수 있는 곳까지 가면서 ny,nx 값을 갱신해가며 이동
-		 5. 종료 조건 : 배열 밖으로 넘어가거나, 이동할 곳에 블록이 위치해 있는 경우
-		 5-1. 같은 값의 블록 : 그 위치의 블록을 2배로 늘린 후 stack 값을 빼버리는 것으로 종료
-		 5-2. 다른 값의 블록 : 이동하기 전의 위치에서 멈추고 그 위치에 블록의 값을 집어넣는 것으로 종료
-		 */
 		switch(dir) {
 		case 0: //상
 			for (int i = 0; i < N; i++) {
@@ -88,7 +75,6 @@ public class Main {
 			for (int i = N-1; i >= 0; i--) {
 				for (int j = N-1; j >= 0; j--) {
 					if(copyar[i][j] != 0) {
-//						System.out.println(1);
 						int ny = i;
 						int nx = j;
 						int sc = copyar[ny][nx];
@@ -157,7 +143,6 @@ public class Main {
 			for (int i = N-1; i >= 0; i--) {
 				for (int j = N-1; j >= 0; j--) {
 					if(copyar[i][j] != 0) {
-//						System.out.println(copyar[i][j]);
 						int ny = i;
 						int nx = j;
 						int sc = copyar[ny][nx];
@@ -189,10 +174,6 @@ public class Main {
 			}
 			break;
 		}
-		
-		
-		
-		
 		if(count > 1) { // 5회 이동
 			move(count-1,0,copyar);
 			move(count-1,1,copyar);
