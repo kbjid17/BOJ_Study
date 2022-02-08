222e8b3a8ee79433e8e8ff09cb3b664ad3a279be
Your Name <someone@aaa.com>
2022-02-08 10:18:37 +0900

    220208 Commit

---
+++ src/prob_그대로출력하기_11718_220207/Main.java
@@ -0,0 +1,17 @@
+package prob_그대로출력하기_11718_220207;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+		String str = "";
+		 
+		while ((str = br.readLine()) != null) {
+		    System.out.println(str);
+		}
+	}
+}
