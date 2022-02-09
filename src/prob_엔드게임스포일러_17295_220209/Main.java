79f78f64d7498474593b0a208b76111f338f0f38
Your Name <someone@aaa.com>
2022-02-09 10:09:42 +0900

    220209 commit

---
+++ src/prob_엔드게임스포일러_17295_220209/Main.java
@@ -0,0 +1,16 @@
+package prob_엔드게임스포일러_17295_220209;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		
+		System.out.println("문제의 답을 출력한다.");
+
+	}
+
+}
