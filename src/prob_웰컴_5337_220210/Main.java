f33ab13dd91aacf75050319913dbf90b0f7e9d3f
Your Name <someone@aaa.com>
2022-02-11 10:12:06 +0900

    220211 commit - 프로젝트로 인해 간단한 구현 문제 해결

---
+++ src/prob_웰컴_5337_220210/Main.java
@@ -0,0 +1,17 @@
+package prob_웰컴_5337_220210;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 .  .   .
+|  | _ | _. _ ._ _  _
+|/\|(/.|(_.(_)[ | )(/.
+		 */
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+
+	}
+
+}
