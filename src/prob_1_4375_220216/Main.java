b1411f63e371b4fed08fc9e81e0d91a19749fe4f
Your Name <someone@aaa.com>
2022-02-16 20:59:48 +0900

    220216 Commit -> 이거 왜 안됨

---
+++ src/prob_1_4375_220216/Main.java
@@ -0,0 +1,27 @@
+package prob_1_4375_220216;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+int n, a, len;
+		
+		String str = null;
+		while( (str = br.readLine()) != null ) {
+			n = Integer.parseInt( str );
+			a = 1;
+			len = 1;
+			
+			while( true ) {
+				if( a%n == 0 ) break;
+				len++;
+				a = ( (a * 10) + 1 ) % n;
+			}
+			System.out.println( len );
+		}
+	}
+}
