fade1543d92a45d592f19ce64f922247889a68ec
김범주 <kbjid17@naver.com>
2022-02-23 14:18:05 +0900

    220222 Commit

---
+++ src/prob_저작권_2914_220222/Main.java
@@ -0,0 +1,18 @@
+package prob_저작권_2914_220222;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		System.out.println(a*b-(a-1));
+	}
+
+}
