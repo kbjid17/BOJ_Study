ad52cf3c8a4971cd2c619dcc17485b1475f0dacd
김범주 <kbjid17@naver.com>
2022-03-10 09:56:32 +0900

    220308 Commit - 재귀적 dp로 품(40분)

--- src/prob_파이프옮기기1_17070_220308/Main_dp_재귀.java
+++ src/prob_파이프옮기기1_17070_220308/Main_dp_재귀.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_dp {
+public class Main_dp_재귀 {
 	static int[][] ar;
 	static int N,ans;
 	public static void main(String[] args) throws Exception{
