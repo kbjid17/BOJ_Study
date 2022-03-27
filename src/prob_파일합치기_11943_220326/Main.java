197039f3a7aee6537da7ef94ad1e1bee95b8201d
김범주 <kbjid17@naver.com>
2022-03-27 14:45:16 +0900

    220327 Commit - 아니 왜 설명을 못하겠냐

--- src/prob_파일합치기_11943_220326/Main.java
+++ src/prob_파일합치기_11943_220326/Main.java
@@ -22,12 +22,14 @@ public class Main {
 				ar[i] = Integer.parseInt(st.nextToken());
 			}
 			dp = new long[N][N];
-			
+			///ij
+
 			for (int j = 1; j < N; j++) {
-				for (int i = j-1; i >= 0; i--) {
+				for (int i = j-1; i >= 0; i--) { //[0][2]
 					// i와 j의 차이만큼 수를 돌려봐야 함.
 					dp[i][j] = Long.MAX_VALUE;
-					for (int d = 0; d < j-i; d++) {
+					for (int d = 0; d < j-i; d++) { //1 max(dp[0][0] + dp[1][2] // dp[0][1] + dp[2][2] 
+						// [0][3] 구하기, dp[0][0] + dp[1][3] // dp[0][1] + dp[2][3] // dp[0][2] + dp[3][3]
 						dp[i][j] = Math.min(dp[i][j], dp[i][i+d] + dp[i+d+1][j]);
 					}
 					for (int k = i; k <= j; k++) {
@@ -38,11 +40,11 @@ public class Main {
 			}
 			System.out.println(dp[0][N-1]);
 		}
-		
+		// A : 40, B : 30, C : 30, D : 50
 		/*
 		 	A	B	C		D
 		 	
-		 A	0	AB	BC+ABC	AB+CD+ABCD
+		 A	0	AB	BC+ABC	AB+CD+ABCD [0][3]
 		 
 		 B		0	BC		BC+BCD
 		 
@@ -50,7 +52,5 @@ public class Main {
 		 
 		 D					0
 		 */
-
 	}
-
 }
