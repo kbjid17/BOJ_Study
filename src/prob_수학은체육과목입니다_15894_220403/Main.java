60b4b41ed503e63ca5c0b18e2d4da2a56bf4f2aa
김범주 <kbjid17@naver.com>
2022-04-03 11:51:03 +0900

    220403 Commit - 플젝하자

---
+++ src/prob_수학은체육과목입니다_15894_220403/Main.java
@@ -0,0 +1,17 @@
+package prob_수학은체육과목입니다_15894_220403;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		System.out.println(Long.parseLong(br.readLine()) * 4);
+	}
+
+}
