ecf87e73c4513a9826ab1a775f8ce5f38e2f51f0
김범주 <kbjid17@naver.com>
2022-04-18 23:32:12 +0900

    220418 Commit - BigInteger는 나머지 연산을 하는 새로운 방법이 있더라!

---
+++ src/prob_큰수big_14928_220418/Main.java
@@ -0,0 +1,16 @@
+package prob_큰수big_14928_220418;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main (String[] args) {
+    	Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        long temp = 0;
+        for (int i = 0; i < input.length(); i++) {
+        	temp = (temp * 10 + (input.charAt(i) - '0')) % 20000303;
+        }
+        
+        System.out.println(temp);
+    }
+}
