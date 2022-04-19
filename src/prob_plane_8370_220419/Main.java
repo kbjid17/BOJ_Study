d805e52f8259ad8d824db20fab3fd3115bb53eb6
김범주 <kbjid17@naver.com>
2022-04-19 22:40:04 +0900

    220419 Commit - 첫 영어 문제.

---
+++ src/prob_plane_8370_220419/Main.java
@@ -0,0 +1,16 @@
+package prob_plane_8370_220419;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		System.out.println((Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken())) + (Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken())));
+	}
+
+}
