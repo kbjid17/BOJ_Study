a6e4251995fef0f10f062fd370e25c4e8837b8b6
김범주 <kbjid17@naver.com>
2022-04-21 10:14:38 +0900

    220421 Commit - 피그마 만들기 힘들다.....

---
+++ src/prob_홍익대학교_16394_220421/Main.java
@@ -0,0 +1,14 @@
+package prob_홍익대학교_16394_220421;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println(Integer.parseInt(br.readLine()) - 1946);
+	}
+
+}
