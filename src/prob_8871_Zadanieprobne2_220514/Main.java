6c6b210e76355ecd96efdd450f4cdfda53a6e91a
김범주 <kbjid17@naver.com>
2022-05-14 23:28:56 +0900

    220514 Commit - 새로운 게임2 푸는중

---
+++ src/prob_8871_Zadanieprobne2_220514/Main.java
@@ -0,0 +1,15 @@
+package prob_8871_Zadanieprobne2_220514;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		System.out.println((n+1)*2 + " " + (n+1)*3);
+
+	}
+
+}
