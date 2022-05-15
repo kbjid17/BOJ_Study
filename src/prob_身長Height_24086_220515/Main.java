058656017eecff6e516a71dec0e4b3428085fe3a
김범주 <kbjid17@naver.com>
2022-05-16 00:31:44 +0900

    220515 Commit - 새로운게임 해결 실패

---
+++ src/prob_身長Height_24086_220515/Main.java
@@ -0,0 +1,17 @@
+package prob_身長Height_24086_220515;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+
+		System.out.println(b-a);
+	}
+
+}
