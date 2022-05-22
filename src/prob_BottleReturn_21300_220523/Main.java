bba016d1c82c81260d00895b1d93e6da1648aae1
김범주 <kbjid17@naver.com>
2022-05-23 03:38:12 +0900

    220522 Commit - 사다리 조작 문제 못품

---
+++ src/prob_BottleReturn_21300_220523/Main.java
@@ -0,0 +1,24 @@
+package prob_BottleReturn_21300_220523;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		int f = Integer.parseInt(st.nextToken());
+
+		System.out.println((a+b+c+d+e+f)*5);
+	}
+
+}
