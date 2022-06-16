eb8790381712f5cb417fbabc114f65f39e4f7f2f
김범주 <kbjid17@naver.com>
2022-06-17 00:04:37 +0900

    220616 Commit - 내일 부산은행 역량평가!

---
+++ src/prob_단어길이재기_2743_220616/Main.java
@@ -0,0 +1,15 @@
+package prob_단어길이재기_2743_220616;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println(br.readLine().length());
+
+	}
+
+}
