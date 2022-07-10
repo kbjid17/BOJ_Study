a8ebe40f62900e43223b4e998c7db9785eb5200f
김범주 <kbjid17@naver.com>
2022-07-10 23:27:31 +0900

    220710 Commit

--- src/prob_크냐_4101_220710/Main.java
+++ src/prob_크냐_4101_220710/Main.java
@@ -1,9 +1,22 @@
 package prob_크냐_4101_220710;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if(a == 0 && b == 0) return;
+			System.out.println(a > b ? "Yes" : "No");
+		}
 
 	}
 
