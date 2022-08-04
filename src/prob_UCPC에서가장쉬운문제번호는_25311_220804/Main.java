f9cf3c176dcc8874ac7bc7f141dbdf60da56c0c4
김범주 <kbjid17@naver.com>
2022-08-04 09:58:38 +0900

    220804 Commit

---
+++ src/prob_UCPC에서가장쉬운문제번호는_25311_220804/Main.java
@@ -0,0 +1,16 @@
+package prob_UCPC에서가장쉬운문제번호는_25311_220804;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str=  br.readLine();
+		System.out.println("A");
+
+	}
+
+}
