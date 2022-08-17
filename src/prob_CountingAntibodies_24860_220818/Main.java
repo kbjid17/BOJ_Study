e5d25524b3ecf38a38dc1afa9ac3482a69607292
김범주 <kbjid17@naver.com>
2022-08-18 00:29:35 +0900

    220817 Commit

---
+++ src/prob_CountingAntibodies_24860_220818/Main.java
@@ -0,0 +1,34 @@
+package prob_CountingAntibodies_24860_220818;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder(br.readLine())
+                .append(" ").append(br.readLine())
+                .append(" ").append(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(sb.toString());
+        final long vk = Long.parseLong(st.nextToken());
+        final long jk = Long.parseLong(st.nextToken());
+        final long vl = Long.parseLong(st.nextToken());
+        final long jl = Long.parseLong(st.nextToken());
+        final long vh = Long.parseLong(st.nextToken());
+        final long dh = Long.parseLong(st.nextToken());
+        final long jh = Long.parseLong(st.nextToken());
+
+        final long heavyChain = vh * dh * jh;
+        final long lightChainK = vk * jk;
+        final long lightChainL = vl * jl;
+        final long totalVariant = heavyChain * (lightChainK + lightChainL);
+
+        // 출력
+        System.out.print(totalVariant);
+
+	}
+
+}
