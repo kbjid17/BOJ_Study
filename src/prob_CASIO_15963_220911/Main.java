19ef76be2a8864a276c9c7d12f5e1ec8718890b5
김범주 <kbjid17@naver.com>
2022-09-11 11:22:43 +0900

    220911 Commit

---
+++ src/prob_CASIO_15963_220911/Main.java
@@ -0,0 +1,21 @@
+package prob_CASIO_15963_220911;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
+
+		if(a == b) System.out.println(1);
+		else System.out.println(0);
+	}
+
+}
